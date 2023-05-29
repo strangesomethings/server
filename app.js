4a017ddcdb22a93c03bcaf57c0fde6d8bf565960
chk223 <104356399+chk223@users.noreply.github.com>
2023-05-30 04:42:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require("dotenv").config()
+const dotenv = require('dotenv')
+const {secretKey} = require('./config/config')
+dotenv.config()
 const express = require("express")
 const http = require("http")
 const app = express()
@@ -7,8 +9,9 @@ const server = http.createServer(app)
 const PORT = process.env.PORT
 app.use(express.urlencoded({extended: true}))
 const cors = require("cors")
-// const test = require('./test/test')
-// app.use("/",test)
+const regist = require('./apis/regist')
+const login = require('./apis/login')
+app.use("/",regist,login)
 app.use(cors({
     origin: 'localhost:3000',//여기 클라이언트쪽 포트 적기.
     Credential: 'true'
